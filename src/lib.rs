824612dd732a91f8e1ac27bb8c10d310036c287f
David Wallace Croft <david@CroftSoft.com>
2023-08-07 20:41:13 -0500

    removed flex

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,22 @@ pub fn app(cx: Scope) -> Element {
 
   cx.render(rsx!(
     div {
-      class: "flex justify-center p-2 mt-5",
+      class: "justify-center p-2 mt-5",
       h1 {
         class: "mb-8 text-4xl font-light",
-        "High-Five counter: {count}" }
-      button {
-        class: "mb-4 mr-2 text-white bg-blue-500 border-0 rounded py-1 px-4 focus:outline-none hover:bg-gray-300",
-        onclick: move |_| count += 1, "Up high!",
+        "High-Five counter: {count}",
       }
       button {
-        class: "mb-4 text-white bg-blue-500 border-0 rounded py-1 px-4 focus:outline-none hover:bg-gray-300",
-        onclick: move |_| count -= 1, "Down low!",
+        class: "mb-4 mr-2 text-white bg-blue-500 border-0 rounded py-1 px-4
+          focus:outline-none hover:bg-gray-300",
+        onclick: move |_| count -= 1,
+        "Down low!",
+      }
+      button {
+        class: "mb-4 text-white bg-blue-500 border-0 rounded py-1 px-4
+          focus:outline-none hover:bg-gray-300",
+        onclick: move |_| count += 1,
+        "Up high!",
       }
     }
   ))
