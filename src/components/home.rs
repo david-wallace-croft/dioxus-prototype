597fad05506143006fd8ade253ed59af93b0501d
David Wallace Croft <david@CroftSoft.com>
2023-08-23 07:36:39 -0500

    canvas

--- src/components/home.rs
+++ src/components/home.rs
@@ -1,7 +1,12 @@
 use dioxus::prelude::*;
+// https://docs.rs/dioxus-hooks/latest/dioxus_hooks/
+// use dioxus::hooks::*;
 
 #[allow(non_snake_case)]
 pub fn Home(cx: Scope) -> Element {
+  // https://github.com/DioxusLabs/dioxus/discussions/999
+  // https://github.com/DioxusLabs/dioxus/blob/master/packages/hooks/src/useeffect.rs
+  use_effect(cx, (), |()| async {});
   render! {
     div {
       class: "box",
@@ -12,6 +17,11 @@ pub fn Home(cx: Scope) -> Element {
     p {
       class: "app-home",
       "This is the home page."
+    }
+    canvas {
+      height: "600",
+      style: "cursor: crosshair",
+      width: "600",
     }
     }
   }
