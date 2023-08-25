85380c115dcef802da99363b3473a8fda8d26b0c
David Wallace Croft <david@CroftSoft.com>
2023-08-25 07:43:00 -0500

    paint canvas background

--- src/components/home.rs
+++ src/components/home.rs
@@ -1,4 +1,7 @@
 use dioxus::prelude::*;
+use std::time::Duration;
+use wasm_bindgen::{JsCast, JsValue};
+use web_sys::{window, CanvasRenderingContext2d, HtmlCanvasElement};
 // https://docs.rs/dioxus-hooks/latest/dioxus_hooks/
 // use dioxus::hooks::*;
 
@@ -7,7 +10,28 @@ pub fn Home(cx: Scope) -> Element {
   // https://github.com/DioxusLabs/dioxus/discussions/999
   // https://github.com/DioxusLabs/dioxus/blob/master/packages/hooks/src/useeffect.rs
   use_effect(cx, (), |()| async {
-    log::info!("logging test");
+    let window = window().expect("global window does not exists");
+    let document = window.document().expect("expecting a document on window");
+    let html_canvas_element = document
+      .get_element_by_id("game-of-life-canvas")
+      .expect("expecting a canvas in the document")
+      .dyn_into::<HtmlCanvasElement>()
+      .unwrap();
+    let canvas_context = html_canvas_element
+      .get_context("2d")
+      .unwrap()
+      .unwrap()
+      .dyn_into::<CanvasRenderingContext2d>()
+      .unwrap();
+    let canvas_height: f64 = html_canvas_element.height() as f64;
+    let canvas_width: f64 = html_canvas_element.width() as f64;
+    let fill_style: JsValue = JsValue::from_str("blue");
+    loop {
+      log::info!("logging test");
+      canvas_context.set_fill_style(&fill_style);
+      canvas_context.fill_rect(0., 0., canvas_width, canvas_height);
+      async_std::task::sleep(Duration::from_millis(1_000)).await;
+    }
   });
   render! {
     div {
@@ -22,6 +46,7 @@ pub fn Home(cx: Scope) -> Element {
     }
     canvas {
       height: "600",
+      id: "game-of-life-canvas",
       style: "cursor: crosshair",
       width: "600",
     }
