129b63e519aa1076c5cc3fffce1ce590e02dc4b6
David Wallace Croft <david@CroftSoft.com>
2023-09-07 06:54:35 -0500

    function extraction

--- src/components/home.rs
+++ src/components/home.rs
@@ -12,41 +12,36 @@ use web_sys::{window, CanvasRenderingContext2d, HtmlCanvasElement};
 
 const CANVAS_ID: &str = "home-page-canvas";
 
+struct Color {
+  blue: u8,
+  green: u8,
+  red: u8,
+}
+
+impl Color {
+  fn to_fill_style(&self) -> JsValue {
+    let Color {
+      blue,
+      green,
+      red,
+    } = self;
+    let rgb: String = format!("rgb({red}, {green}, {blue})");
+    // log::info!("{rgb}");
+    JsValue::from_str(&rgb)
+  }
+}
+
 #[allow(non_snake_case)]
 pub fn Home(cx: Scope) -> Element {
   // https://github.com/DioxusLabs/dioxus/discussions/999
   // https://github.com/DioxusLabs/dioxus/blob/master/packages/hooks/src/useeffect.rs
   use_effect(cx, (), |()| async {
-    let window = window().expect("global window does not exists");
-    let document = window.document().expect("expecting a document on window");
-    let html_canvas_element = document
-      .get_element_by_id(CANVAS_ID)
-      .expect("expecting a canvas in the document")
-      .dyn_into::<HtmlCanvasElement>()
-      .unwrap();
-    let canvas_context = html_canvas_element
-      .get_context("2d")
-      .unwrap()
-      .unwrap()
-      .dyn_into::<CanvasRenderingContext2d>()
-      .unwrap();
-    let canvas_height: f64 = html_canvas_element.height() as f64;
-    let canvas_width: f64 = html_canvas_element.width() as f64;
-    let mut rng: ThreadRng = rand::thread_rng();
-    let d256: Uniform<u8> = Uniform::from(0..=255);
-    let mut r: u8 = d256.sample(&mut rng);
-    let mut g: u8 = d256.sample(&mut rng);
-    let mut b: u8 = d256.sample(&mut rng);
+    let mut color: Color = generate_random_color();
     loop {
-      let rgb: String = format!("rgb({r}, {g}, {b})");
-      // log::info!("{rgb}");
-      let fill_style: JsValue = JsValue::from_str(&rgb);
-      canvas_context.set_fill_style(&fill_style);
-      canvas_context.fill_rect(0., 0., canvas_width, canvas_height);
+      let fill_style: JsValue = color.to_fill_style();
+      paint_background(&fill_style);
       async_std::task::sleep(Duration::from_millis(17u64)).await;
-      r = drift(r);
-      g = drift(g);
-      b = drift(b);
+      drift_color(&mut color);
     }
   });
   render! {
@@ -76,12 +71,31 @@ pub fn Home(cx: Scope) -> Element {
   }
 }
 
-fn drift(primary_color: u8) -> u8 {
+fn drift_color(color: &mut Color) {
+  color.blue = drift_primary_color(color.blue);
+  color.green = drift_primary_color(color.green);
+  color.red = drift_primary_color(color.red);
+}
+
+fn drift_primary_color(primary_color: u8) -> u8 {
   let range: RangeInclusive<i8> = -1..=1;
   let die: Uniform<i8> = Uniform::from(range);
   let mut rng: ThreadRng = rand::thread_rng();
   let delta: i8 = die.sample(&mut rng);
   primary_color.saturating_add_signed(delta)
+}
+
+fn generate_random_color() -> Color {
+  let mut rng: ThreadRng = rand::thread_rng();
+  let d256: Uniform<u8> = Uniform::from(0..=255);
+  let red: u8 = d256.sample(&mut rng);
+  let green: u8 = d256.sample(&mut rng);
+  let blue: u8 = d256.sample(&mut rng);
+  Color {
+    blue,
+    green,
+    red,
+  }
 }
 
 fn on_click(event: Event<MouseData>) {
@@ -98,4 +112,24 @@ fn on_mouse_out(event: Event<MouseData>) {
 
 fn on_wheel(event: Event<WheelData>) {
   log::info!("onwheel Event: {event:?}");
+}
+
+fn paint_background(fill_style: &JsValue) {
+  let window = window().expect("global window does not exists");
+  let document = window.document().expect("expecting a document on window");
+  let html_canvas_element = document
+    .get_element_by_id(CANVAS_ID)
+    .expect("expecting a canvas in the document")
+    .dyn_into::<HtmlCanvasElement>()
+    .unwrap();
+  let canvas_context = html_canvas_element
+    .get_context("2d")
+    .unwrap()
+    .unwrap()
+    .dyn_into::<CanvasRenderingContext2d>()
+    .unwrap();
+  let canvas_height: f64 = html_canvas_element.height() as f64;
+  let canvas_width: f64 = html_canvas_element.width() as f64;
+  canvas_context.set_fill_style(&fill_style);
+  canvas_context.fill_rect(0., 0., canvas_width, canvas_height);
 }
