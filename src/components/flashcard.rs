6c68ea0f29e3f914c448aada9a3fd96d6077bacc
David Wallace Croft <david@CroftSoft.com>
2023-09-20 05:45:30 -0500

    buttons

--- src/components/flashcard.rs
+++ src/components/flashcard.rs
@@ -2,23 +2,50 @@ use dioxus::prelude::*;
 
 #[allow(non_snake_case)]
 pub fn Flashcard(cx: Scope) -> Element {
-  let mut count = use_state(cx, || 0);
+  // let mut count = use_state(cx, || 0);
   render! {
     div {
-      class: "app-high-five box",
-      h1 {
-        "High-Five counter: {count}",
-      }
-      button {
-        class: "app-high-five",
-        onclick: move |_| count -= 1,
-        "Down low!",
-      }
-      button {
-        class: "app-high-five",
-        onclick: move |_| count += 1,
-        "Up high!",
-      }
+      class: "box",
+    div {
+    button {
+    "SHOW"
+    }
+    }
+    div {
+    "8 x 6 = ?"
+    }
+    div {
+    button {
+    "0"
+    }
+    button {
+    "4"
+    }
+    button {
+    "12"
+    }
+    button {
+    "14"
+    }
+    button {
+    "42"
+    }
+    button {
+    "44"
+    }
+    button {
+    "48"
+    }
+    button {
+    "55"
+    }
+    button {
+    "84"
+    }
+    button {
+    "99"
+    }
+    }
     }
   }
 }
