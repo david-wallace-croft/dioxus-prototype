adbc649ccf32770eddb5caa997a47426b40285c0
David Wallace Croft <david@CroftSoft.com>
2023-09-23 13:49:37 -0500

    flashcard CSS

--- src/components/flashcard.rs
+++ src/components/flashcard.rs
@@ -13,37 +13,48 @@ pub fn Flashcard(cx: Scope) -> Element {
     }
     }
     div {
+      class: "app-question",
     "8 x 6 = ?"
     }
     div {
     button {
+      class: "app-answer",
     "0"
     }
     button {
+      class: "app-answer",
     "4"
     }
     button {
+      class: "app-answer",
     "12"
     }
     button {
+      class: "app-answer",
     "14"
     }
     button {
+      class: "app-answer",
     "42"
     }
     button {
+      class: "app-answer",
     "44"
     }
     button {
+      class: "app-answer",
     "48"
     }
     button {
+      class: "app-answer",
     "55"
     }
     button {
+      class: "app-answer",
     "84"
     }
     button {
+      class: "app-answer",
     "99"
     }
     }
