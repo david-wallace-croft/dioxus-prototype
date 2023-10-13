e96ebacfafb5467e778c6c4f68e7ec2f36f1143e
David Wallace Croft <david@CroftSoft.com>
2023-10-13 07:43:11 -0500

    retirement CSS grid

--- src/components/home.rs
+++ src/components/home.rs
@@ -1,3 +1,4 @@
+use crate::components::retirement::Retirement;
 use dioxus::prelude::*;
 
 #[allow(non_snake_case)]
@@ -19,6 +20,8 @@ pub fn Home(cx: Scope) -> Element {
     }
     "."
     }
+    // TODO: Temporarily putting this on the homepage to make development easier
+    Retirement {}
     }
   }
 }
--- src/components/retirement/mod.rs
+++ src/components/retirement/mod.rs
@@ -1,9 +1,18 @@
 use dioxus::prelude::*;
 
+static INVESTMENT_INTEREST: &str = "10.0";
+static INVESTMENT_YEARS: &str = "50.0";
+static RETIREMENT_INCOME: &str = "100000.0";
+static RETIREMENT_INFLATION: &str = "1.0";
+static RETIREMENT_INTEREST: &str = "10.0";
+static RETIREMENT_TAX_RATE: &str = "10.0";
+
 #[allow(non_snake_case)]
 pub fn Retirement(cx: Scope) -> Element {
-  let desired_income: &UseState<String> =
-    use_state(cx, || "100000.0".to_string());
+  let retirement_income: &UseState<String> =
+    use_state(cx, || RETIREMENT_INCOME.to_string());
+  let investment_years: &UseState<String> =
+    use_state(cx, || INVESTMENT_YEARS.to_string());
   render! {
   div {
     class: "app-retirement box",
@@ -13,16 +22,43 @@ pub fn Retirement(cx: Scope) -> Element {
   }
   div {
     class: "app-form",
+
   span {
-      "Desired annual retirement income (present value, after taxes)"
+    style: "white-space: pre-line",
+    "Desired annual retirement income\n (present value, after taxes)"
   }
   input {
-    value: "{desired_income}",
-    oninput: move |evt| desired_income.set(evt.value.clone()),
-  }
+    size: "10",
+    oninput: move |evt| retirement_income.set(evt.value.clone()),
+    r#type: "text",
+    value: "{retirement_income}",
   }
   span {
-    "{desired_income}"
+    style: "align-self: center",
+    "dollars ($)"
+  }
+
+  span {
+    style: "white-space: pre-line",
+    "Years until retirement\n (usually at 67 years of age)"
+  }
+  input {
+    size: "10",
+    oninput: move |evt| investment_years.set(evt.value.clone()),
+    r#type: "text",
+    value: "{investment_years}",
+  }
+  span {
+    style: "align-self: center",
+    "years"
+  }
+
+  }
+  p {
+    "{retirement_income}"
+  }
+  p {
+    "{investment_years}"
   }
   }
   }
