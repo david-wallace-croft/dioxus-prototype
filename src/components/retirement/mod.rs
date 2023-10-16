c7080de161bb165a5247ca9db5ebf95543b9e366
David Wallace Croft <david@CroftSoft.com>
2023-10-16 14:49:14 -0500

    retirement calculation

--- src/components/retirement/mod.rs
+++ src/components/retirement/mod.rs
@@ -1,3 +1,4 @@
+use com_croftsoft_core::math::finance_lib::PeriodicSavingsNeeded;
 use dioxus::prelude::*;
 
 static INVESTMENT_INTEREST: &str = "10.0";
@@ -9,18 +10,18 @@ static RETIREMENT_TAX_RATE: &str = "10.0";
 
 #[allow(non_snake_case)]
 pub fn Retirement(cx: Scope) -> Element {
-  let retirement_income: &UseState<String> =
-    use_state(cx, || RETIREMENT_INCOME.to_string());
-  let investment_years: &UseState<String> =
-    use_state(cx, || INVESTMENT_YEARS.to_string());
   let investment_interest: &UseState<String> =
     use_state(cx, || INVESTMENT_INTEREST.to_string());
+  let investment_years: &UseState<String> =
+    use_state(cx, || INVESTMENT_YEARS.to_string());
+  let retirement_income: &UseState<String> =
+    use_state(cx, || RETIREMENT_INCOME.to_string());
+  let retirement_inflation: &UseState<String> =
+    use_state(cx, || RETIREMENT_INFLATION.to_string());
   let retirement_interest: &UseState<String> =
     use_state(cx, || RETIREMENT_INTEREST.to_string());
   let retirement_tax_rate: &UseState<String> =
-    use_state(cx, || RETIREMENT_INTEREST.to_string());
-  let retirement_inflation: &UseState<String> =
-    use_state(cx, || RETIREMENT_INFLATION.to_string());
+    use_state(cx, || RETIREMENT_TAX_RATE.to_string());
   render! {
   div {
     class: "app-retirement box",
@@ -117,23 +118,79 @@ pub fn Retirement(cx: Scope) -> Element {
 
   }
   p {
-    "{retirement_income}"
+    "You would need to invest "
+    "{calculate_required_annual_investment_from_state(
+        investment_interest,
+        investment_years,
+        retirement_income,
+        retirement_inflation,
+        retirement_interest,
+        retirement_tax_rate,
+      )}"
+    " each year."
   }
   p {
-    "{investment_years}"
-  }
-  p {
-    "{investment_interest}"
-  }
-  p {
-    "{retirement_interest}"
-  }
-  p {
-    "{retirement_tax_rate}"
-  }
-  p {
-    "{retirement_inflation}"
+    "This calculator does not factor in social security income."
+    br {}
+    "Click "
+    a {
+      href: "https://www.bankrate.com/retirement/retirement-plan-calculator/",
+      target: "_blank",
+      "here"
+    }
+    " for a calculator that includes social security income."
   }
   }
   }
+}
+
+fn calculate_required_annual_investment(
+  desired_savings_interest_income: f64,
+  years_of_saving: f64,
+  investment_interest_rate: f64,
+  savings_interest_rate: f64,
+  tax_rate: f64,
+  inflation_rate: f64,
+) -> f64 {
+  let savings: f64 = desired_savings_interest_income * (1.0 + inflation_rate)
+    / (savings_interest_rate * (1.0 - tax_rate) - inflation_rate);
+  if years_of_saving == 0.0 {
+    return savings;
+  }
+  let future_value_savings =
+    savings * (1.0 + inflation_rate).powf(years_of_saving);
+  PeriodicSavingsNeeded {
+    future_value: future_value_savings,
+    interest_rate: investment_interest_rate,
+    time_periods: years_of_saving,
+  }
+  .calculate()
+}
+
+fn calculate_required_annual_investment_from_state(
+  investment_interest: &UseState<String>,
+  investment_years: &UseState<String>,
+  retirement_income: &UseState<String>,
+  retirement_inflation: &UseState<String>,
+  retirement_interest: &UseState<String>,
+  retirement_tax_rate: &UseState<String>,
+) -> f64 {
+  let desired_savings_interest_income: f64 = parse(retirement_income);
+  let years_of_saving: f64 = parse(investment_years);
+  let investment_interest_rate: f64 = parse(investment_interest) / 100.;
+  let savings_interest: f64 = parse(retirement_interest) / 100.;
+  let tax_rate: f64 = parse(retirement_tax_rate) / 100.;
+  let inflation_rate: f64 = parse(retirement_inflation) / 100.;
+  calculate_required_annual_investment(
+    desired_savings_interest_income,
+    years_of_saving,
+    investment_interest_rate,
+    savings_interest,
+    tax_rate,
+    inflation_rate,
+  )
+}
+
+fn parse(state: &UseState<String>) -> f64 {
+  state.get().parse().unwrap_or(0.)
 }
