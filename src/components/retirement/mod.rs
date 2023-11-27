fd97e1c8e7b2f8d9450d5f673a93609100716ba6
David Wallace Croft <david@CroftSoft.com>
2023-11-27 07:56:41 -0600

    moved fn to_dollars to crate

--- src/components/retirement/mod.rs
+++ src/components/retirement/mod.rs
@@ -2,9 +2,8 @@ use self::reset_button::ResetButton;
 use super::translator::Translator;
 use crate::components::language_select::LanguageSelector;
 use com_croftsoft_core::math::finance_lib::PeriodicSavingsNeeded;
+use com_croftsoft_lib_string::to_dollars;
 use dioxus::prelude::*;
-use std::iter::Rev;
-use std::str::Chars;
 
 mod reset_button;
 
@@ -398,36 +397,4 @@ fn parse_input(value: &str) -> Option<f64> {
 
 fn parse_state(state: &UseState<String>) -> f64 {
   parse_input(state.get()).unwrap_or(0.)
-}
-
-fn to_comma_separated(value: u64) -> String {
-  let value_as_string: String = value.to_string();
-  let reversed_without_commas: Rev<Chars> = value_as_string.chars().rev();
-  let mut reversed_with_commas: String = "".to_string();
-  for (i, c) in reversed_without_commas.enumerate() {
-    if (i > 0) && (i % 3 == 0) {
-      reversed_with_commas.push(',');
-    }
-    reversed_with_commas.push(c);
-  }
-  let comma_separated: String = to_reverse_string(reversed_with_commas);
-  comma_separated
-}
-
-fn to_dollars(amount: f64) -> String {
-  let rounded_amount: f64 = amount.round();
-  let integer_amount: i64 = rounded_amount as i64;
-  let positive_amount: u64 = integer_amount.unsigned_abs();
-  let comma_separated_string: String = to_comma_separated(positive_amount);
-  let mut dollars: String = "".to_owned();
-  if integer_amount.is_negative() {
-    dollars.push('-');
-  }
-  dollars.push('$');
-  dollars += &comma_separated_string;
-  dollars
-}
-
-fn to_reverse_string(s: String) -> String {
-  s.chars().rev().collect::<String>()
 }
