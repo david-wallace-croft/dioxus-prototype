949fdb7e4a7e1d261ed4a1fb1c2ea17189b0fe5a
David Wallace Croft <david@CroftSoft.com>
2023-12-12 16:10:45 -0600

    OIDC login test

--- src/components/login/mod.rs
+++ src/components/login/mod.rs
@@ -3,14 +3,29 @@ use dioxus::prelude::*;
 #[allow(non_snake_case)]
 pub fn Login(cx: Scope) -> Element {
   render! {
-    div {
-      class: "app-login box",
-    h1 {
-      "Login"
-    }
-    p {
-      "Not yet implemented"
-    }
-    }
+  div {
+    class: "app-login box",
+  h1 {
+  "Login"
+  }
+  p {
+  "Click on the following to log into the application:"
+  br { }
+  a {
+    href: "https://73yqz7i9oi-69cbfasnr72e657dj72qu0cprp\
+      .auth.us-east-1.amazoncognito.com/\
+      login?client_id=69cbfasnr72e657dj72qu0cprp\
+      &response_type=code\
+      &scope=openid\
+      &redirect_uri=http%3A%2F%2Flocalhost%3A8080",
+  "https://73yqz7i9oi-69cbfasnr72e657dj72qu0cprp\
+    .auth.us-east-1.amazoncognito.com/login\
+    ?client_id=69cbfasnr72e657dj72qu0cprp\
+    &response_type=code\
+    &scope=openid\
+    &redirect_uri=http%3A%2F%2Flocalhost%3A8080"
+  }
+  }
+  }
   }
 }
