d2f261e560077120631a8fa47718a5fedcb94685
David Wallace Croft <david@CroftSoft.com>
2024-01-19 13:36:24 -0600

    map path

--- src/bin/prerender.rs
+++ src/bin/prerender.rs
@@ -1,16 +1,33 @@
 use ::dioxus_fullstack::prelude::*;
+use ::dioxus_fullstack::router::FullstackRouterConfig;
+use ::std::path::PathBuf;
 use dioxus_prototype::route::Route;
+
+const STATIC_DIR: &str = "dist";
 
 #[tokio::main]
 async fn main() {
-  pre_cache_static_routes_with_props(
-    &ServeConfigBuilder::new_with_router(
-      dioxus_fullstack::router::FullstackRouterConfig::<Route>::default(),
-    )
-    .assets_path("dist")
-    .incremental(IncrementalRendererConfig::default().static_dir("dist"))
-    .build(),
-  )
-  .await
-  .unwrap();
+  let incremental_renderer_config = IncrementalRendererConfig::default()
+    .map_path(map_path)
+    .static_dir(STATIC_DIR);
+  let fullstack_router_config = FullstackRouterConfig::<Route>::default();
+  let serve_config: ServeConfig<FullstackRouterConfig<Route>> =
+    ServeConfigBuilder::new_with_router(fullstack_router_config)
+      .assets_path(STATIC_DIR)
+      .incremental(incremental_renderer_config)
+      .build();
+  pre_cache_static_routes_with_props(&serve_config)
+    .await
+    .unwrap();
+}
+
+fn map_path(route: &str) -> PathBuf {
+  let mut path = PathBuf::from(STATIC_DIR);
+  let pattern = [
+    '/', '?',
+  ];
+  for segment in route.split(pattern) {
+    path.push(segment);
+  }
+  path
 }
