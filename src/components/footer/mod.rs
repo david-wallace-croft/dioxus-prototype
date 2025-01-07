aca2d369c1734a03975a55a6ecb65e09c40b314b
David Wallace Croft <david@CroftSoft.com>
2025-01-06 19:17:54 -0600

    footer copyright year

--- src/components/footer/mod.rs
+++ src/components/footer/mod.rs
@@ -7,7 +7,7 @@ pub fn Footer() -> Element {
     footer {
       class: "box",
       dangerous_inner_html: "&copy;",
-    " 2023-2024 "
+    " 2023-2025 "
     a {
       href: "https://www.CroftSoft.com/",
       target: "_blank",
--- src/components/template/mod.rs
+++ src/components/template/mod.rs
@@ -1,15 +1,11 @@
 use super::super::components::footer::Footer;
 use super::super::components::nav::Nav;
-// use super::super::components::translator::SharedState;
 use super::super::route::Route;
 use ::dioxus::prelude::*;
 
 #[allow(non_snake_case)]
 #[component]
 pub fn Template() -> Element {
-  // use_shared_state_provider(cx, || SharedState {
-  //   lang: "en".to_string(),
-  // });
   rsx! {
     div {
       class: "frame",
