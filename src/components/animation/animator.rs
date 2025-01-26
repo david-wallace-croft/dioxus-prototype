181454b59d40805c607479ad0ea48744c3a2b05b
David Wallace Croft <david@CroftSoft.com>
2025-01-25 21:01:55 -0600

    Animator

---
+++ src/components/animation/animator.rs
@@ -0,0 +1,126 @@
+use super::color::Color;
+use ::web_sys::wasm_bindgen::JsCast;
+use ::web_sys::{
+  window, CanvasRenderingContext2d, Document, HtmlCanvasElement, Window,
+};
+
+pub struct Animator {
+  canvas_height: f64,
+  canvas_rendering_context_2d: CanvasRenderingContext2d,
+  canvas_width: f64,
+  color: Color,
+  delta_x: f64,
+  delta_y: f64,
+  square_size: f64,
+  x: f64,
+  y: f64,
+}
+
+impl Animator {
+  pub fn new(canvas_id: &str) -> Self {
+    let window: Window = window().expect("global window does not exists");
+
+    let document: Document =
+      window.document().expect("expecting a document on window");
+
+    let html_canvas_element = document
+      .get_element_by_id(canvas_id)
+      .expect("expecting a canvas in the document")
+      .dyn_into::<HtmlCanvasElement>()
+      .unwrap();
+
+    let canvas_rendering_context_2d: CanvasRenderingContext2d =
+      html_canvas_element
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<CanvasRenderingContext2d>()
+        .unwrap();
+
+    let canvas_height: f64 = html_canvas_element.height() as f64;
+
+    let canvas_width: f64 = html_canvas_element.width() as f64;
+
+    let color: Color = Color::generate_random_color();
+
+    let square_size: f64 =
+      100.0_f64.min(canvas_width / 2.).min(canvas_height / 2.);
+
+    let delta_x: f64 = 1.;
+
+    let delta_y: f64 = 1.;
+
+    let x: f64 = -delta_x;
+
+    let y: f64 = -delta_y;
+
+    Self {
+      canvas_height,
+      canvas_rendering_context_2d,
+      canvas_width,
+      color,
+      delta_x,
+      delta_y,
+      square_size,
+      x,
+      y,
+    }
+  }
+
+  pub fn paint(
+    &self,
+    message: &str,
+  ) {
+    self.canvas_rendering_context_2d.set_fill_style_str("black");
+
+    self.canvas_rendering_context_2d.fill_rect(
+      0.,
+      0.,
+      self.canvas_width,
+      self.canvas_height,
+    );
+
+    let fill_style: String = self.color.as_fill_style_string();
+
+    self
+      .canvas_rendering_context_2d
+      .set_fill_style_str(&fill_style);
+
+    self.canvas_rendering_context_2d.fill_rect(
+      self.x,
+      self.y,
+      self.square_size,
+      self.square_size,
+    );
+
+    self.canvas_rendering_context_2d.set_font("30px Verdana");
+
+    self.canvas_rendering_context_2d.set_fill_style_str("white");
+
+    let _ = self.canvas_rendering_context_2d.fill_text(message, 4., 30.);
+  }
+
+  pub fn update(&mut self) {
+    if self.delta_x > 0. {
+      if self.x + self.delta_x + self.square_size > self.canvas_width {
+        self.delta_x = -self.delta_x;
+      }
+    } else if self.x + self.delta_x < 0. {
+      self.delta_x = -self.delta_x;
+    }
+
+    self.x += self.delta_x;
+
+    if self.delta_y > 0. {
+      if self.y + self.delta_y + self.square_size > self.canvas_height {
+        self.delta_y = -self.delta_y;
+      }
+    } else if self.y + self.delta_y < 0. {
+      self.delta_y = -self.delta_y;
+    }
+
+    self.y += self.delta_y;
+
+    self.color = self.color.drift();
+  }
+}
--- src/components/animation/mod.rs
+++ src/components/animation/mod.rs
@@ -1,13 +1,10 @@
-use self::color::Color;
-use ::dioxus::html::geometry::WheelDelta::{self, Lines, Pages, Pixels};
+use self::animator::Animator;
+// use ::dioxus::html::geometry::WheelDelta::{self, Lines, Pages, Pixels};
 use ::dioxus::prelude::*;
 use ::std::time::Duration;
 use ::tracing::info;
-use ::web_sys::wasm_bindgen::JsCast;
-use ::web_sys::{
-  window, CanvasRenderingContext2d, Document, HtmlCanvasElement, Window,
-};
 
+mod animator;
 mod color;
 
 const CANVAS_ID: &str = "home-page-canvas";
@@ -23,9 +20,6 @@ pub fn Animation() -> Element {
 
   let mut click_count_signal: Signal<i32> = use_signal(|| 0);
 
-  let mut color_signal: Signal<Color> =
-    use_signal(Color::generate_random_color);
-
   let mut message_signal: Signal<&str> = use_signal(|| MESSAGE_START);
 
   let mut running_signal: Signal<bool> = use_signal(|| true);
@@ -35,86 +29,19 @@ pub fn Animation() -> Element {
   // https://github.com/DioxusLabs/dioxus/discussions/999
   // https://github.com/DioxusLabs/dioxus/blob/master/packages/hooks/src/use_effect.rs
   use_future(move || {
-    to_owned![color_signal];
     to_owned![message_signal];
     to_owned![running_signal];
     to_owned![update_signal];
     async move {
-      let window: Window = window().expect("global window does not exists");
-
-      let document: Document =
-        window.document().expect("expecting a document on window");
-
-      let html_canvas_element = document
-        .get_element_by_id(CANVAS_ID)
-        .expect("expecting a canvas in the document")
-        .dyn_into::<HtmlCanvasElement>()
-        .unwrap();
-
-      let canvas_context: CanvasRenderingContext2d = html_canvas_element
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<CanvasRenderingContext2d>()
-        .unwrap();
-
-      let canvas_height: f64 = html_canvas_element.height() as f64;
-
-      let canvas_width: f64 = html_canvas_element.width() as f64;
-
-      let square_size: f64 =
-        100.0_f64.min(canvas_width / 2.).min(canvas_height / 2.);
-
-      let mut delta_x: f64 = 1.;
-
-      let mut delta_y: f64 = 1.;
-
-      let mut x: f64 = -delta_x;
-
-      let mut y: f64 = -delta_y;
+      let mut animator = Animator::new(CANVAS_ID);
 
       loop {
         if *running_signal.read() || *update_signal.read() {
           update_signal.set(false);
 
-          if delta_x > 0. {
-            if x + delta_x + square_size > canvas_width {
-              delta_x = -delta_x;
-            }
-          } else if x + delta_x < 0. {
-            delta_x = -delta_x;
-          }
+          animator.update();
 
-          x += delta_x;
-
-          if delta_y > 0. {
-            if y + delta_y + square_size > canvas_height {
-              delta_y = -delta_y;
-            }
-          } else if y + delta_y < 0. {
-            delta_y = -delta_y;
-          }
-
-          y += delta_y;
-
-          let color: Color = *color_signal.read();
-
-          let drift_color: Color = color.drift();
-
-          color_signal.set(drift_color);
-
-          let fill_style: String = color_signal.read().as_fill_style_string();
-
-          paint(
-            &canvas_context,
-            canvas_height,
-            canvas_width,
-            &fill_style,
-            *message_signal.read(),
-            square_size,
-            x,
-            y,
-          );
+          animator.paint(*message_signal.read());
         }
 
         async_std::task::sleep(Duration::from_millis(17u64)).await;
@@ -136,10 +63,10 @@ pub fn Animation() -> Element {
       id: CANVAS_ID,
       // https://docs.rs/dioxus/latest/dioxus/events/index.html
       onblur: move |event| on_blur(event, &mut message_signal, &mut running_signal),
-      onclick: move |event| on_click(event, &mut click_count_signal, &mut color_signal, &mut update_signal),
+      onclick: move |event| on_click(event, &mut click_count_signal, &mut update_signal),
       onfocus: move |event| on_focus(event, &mut message_signal, &mut running_signal, &mut update_signal),
-      onkeydown: move |event| on_key_down(event, &mut color_signal, &mut update_signal),
-      onwheel: move |event| on_wheel(event, &mut color_signal, &mut update_signal),
+      onkeydown: move |event| on_key_down(event, &mut update_signal),
+      onwheel: move |event| on_wheel(event, &mut update_signal),
       tabindex: 0,
       width: "600",
     }
@@ -162,7 +89,6 @@ fn on_blur(
 fn on_click(
   _event: Event<MouseData>,
   click_count_signal: &mut Signal<i32>,
-  _color_signal: &mut Signal<Color>,
   _update_signal: &mut Signal<bool>,
 ) {
   // log::info!("onclick Event: {event:?}");
@@ -197,65 +123,31 @@ fn on_focus(
 
 fn on_key_down(
   _event: Event<KeyboardData>,
-  color_signal: &mut Signal<Color>,
   update_signal: &mut Signal<bool>,
 ) {
-  // log::info!("onkeydown Event: {event:?}");
-
-  let color: Color = *color_signal.read();
-
-  let drift_color: Color = color.drift();
-
-  color_signal.set(drift_color);
-
   update_signal.set(true);
 }
 
 fn on_wheel(
-  event: Event<WheelData>,
-  color_signal: &mut Signal<Color>,
+  _event: Event<WheelData>,
+  // color_signal: &mut Signal<Color>,
   update_signal: &mut Signal<bool>,
 ) {
   // log::info!("onwheel Event: {event:?}");
 
-  let wheel_delta: WheelDelta = event.delta();
+  // let wheel_delta: WheelDelta = event.delta();
 
-  let delta: f64 = match wheel_delta {
-    Lines(lines_vector) => lines_vector.y,
-    Pages(pages_vector) => pages_vector.y,
-    Pixels(pixels_vector) => pixels_vector.y,
-  };
+  // let delta: f64 = match wheel_delta {
+  //   Lines(lines_vector) => lines_vector.y,
+  //   Pages(pages_vector) => pages_vector.y,
+  //   Pixels(pixels_vector) => pixels_vector.y,
+  // };
 
-  let delta: i8 = delta.clamp(-1., 1.) as i8;
+  // let delta: i8 = delta.clamp(-1., 1.) as i8;
 
-  let color: Color = *color_signal.read();
+  // let color: Color = *color_signal.read();
 
-  color_signal.set(color.shift(delta));
+  // color_signal.set(color.shift(delta));
 
   update_signal.set(true);
-}
-
-fn paint(
-  canvas_context: &CanvasRenderingContext2d,
-  canvas_height: f64,
-  canvas_width: f64,
-  fill_style: &str,
-  message: &str,
-  square_size: f64,
-  x: f64,
-  y: f64,
-) {
-  canvas_context.set_fill_style_str("black");
-
-  canvas_context.fill_rect(0., 0., canvas_width, canvas_height);
-
-  canvas_context.set_fill_style_str(fill_style);
-
-  canvas_context.fill_rect(x, y, square_size, square_size);
-
-  canvas_context.set_font("30px Verdana");
-
-  canvas_context.set_fill_style_str("white");
-
-  let _ = canvas_context.fill_text(message, 4., 30.);
 }
