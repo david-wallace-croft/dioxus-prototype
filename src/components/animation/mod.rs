d49417518a8b1724365a5ad8fd607e1e2a4170b9
David Wallace Croft <david@CroftSoft.com>
2025-04-02 21:11:48 -0500

    TODO

--- src/components/animation/mod.rs
+++ src/components/animation/mod.rs
@@ -93,6 +93,7 @@ pub fn Animation() -> Element {
           animator.paint();
         }
 
+        // TODO: Can we replace this with a request_animation_frame?
         async_std::task::sleep(Duration::from_millis(17u64)).await;
       }
     }
